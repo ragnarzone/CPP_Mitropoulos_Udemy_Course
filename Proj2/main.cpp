a0325ae16ee42902f7645706b0bd128494081929
ragnarzone <ragnarzone@gmail.com>
2023-08-13 19:15:22 +0300

    added debug config file and test debug capabilities of vs code

--- Proj2/main.cpp
+++ Proj2/main.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 
 int main(){
+    int num {100};
     cout << "Hello from Proj2" << endl;
+    cout << "Enter your favorite number : ";
+    cin >> num;
+    cout << "Your favorite number is " << num << endl;
     return 0;
 }
